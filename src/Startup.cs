771276fd038499e20638f776c078aef717da76fa
Shawn Wildermuth <shawn@wildermuth.com>
2016-05-13 20:01:52 +0200

    Make Hello World Work

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,10 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
 namespace ASPNETRC2FromScratch
 {
   public class Startup
   {
-    public void Configure()
+    public void Configure(IApplicationBuilder app)
     {
-      
+      app.Run(async ctx => await ctx.Response.WriteAsync($"Hello World! Req: {ctx.Request.Path}"));      
     }
   }  
 }
